7fdd747e07b2b885a93ccacaa69dc9d7c2f11298
Vinay846 <Vinayvin84629@gmail.com>
2020-12-14 17:34:39 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -20,54 +20,54 @@ app.get("/findColleges", async (req, res)=>{
     const course = req.query.course;
     const exam = req.query.exam;
     let result = [];
-    if(isNullOrUndefined(name) && isNullOrUndefined(state) && isNullOrUndefined(city) && isNullOrUndefined(minPackage) &&
-    isNullOrUndefined(maxFees) && isNullOrUndefined(course) && isNullOrUndefined(exams)){
+    if(isNullOrUndefined(name) && isNullOrUndefined(state) && isNullOrUndefined(city) && isNullOrUndefined(minPackage) && isNullOrUndefined(maxFees) && isNullOrUndefined(course) && isNullOrUndefined(exam)){
         res.send(await connection.find());
+    }else{
+        if(!isNullOrUndefined(name)){
+            const listOfName = await connection.find({name:{$regex:name,$options:"$i"}});
+            listOfName.forEach(element => {
+                result.push(element);
+            });    
+        }
+        if(!isNullOrUndefined(state)){
+            const listOfstate = await connection.find({state:{$regex:state,$options:"$i"}});
+            listOfstate.forEach(element => {
+                result.push(element);
+            });    
+        }
+        if(!isNullOrUndefined(city)){
+            const listOfcity = await connection.find({city:{$regex:city,$options:"$i"}});
+            listOfcity.forEach(element => {
+                result.push(element);
+            });    
+        }
+        if(!isNullOrUndefined(minPackage) && minPackage > 0){
+            const listOfminPackage = await connection.find({minPackage:{$gte : minPackage}});
+            listOfminPackage.forEach(element => {
+                result.push(element);
+            });    
+        }
+        if(!isNullOrUndefined(maxFees) && maxFees > 0){
+            const listOfmaxFees = await connection.find({maxFees:{$lte : maxFees}});
+            listOfmaxFees.forEach(element => {
+                result.push(element);
+            });    
+        }
+        if(!isNullOrUndefined(course)){
+            const listOfcourse = await connection.find({course:{$regex:course,$options:"$i"}});
+            listOfcourse.forEach(element => {
+                result.push(element);
+            });    
+        }
+        if(!isNullOrUndefined(exam)){
+            const listOfexams = await connection.find({exam:{$regex:exam,$options:"$i"}});
+            listOfexams.forEach(element => {
+                result.push(element);
+            });    
+        }
+        res.send([...new Set(result)]);
     }
 
-    if(!isNullOrUndefined(name)){
-        const listOfName = await connection.find({name:{$regex:name,$options:"$i"}});
-        listOfName.forEach(element => {
-            result.push(element);
-        });    
-    }
-    if(!isNullOrUndefined(state)){
-        const listOfstate = await connection.find({state:{$regex:state,$options:"$i"}});
-        listOfstate.forEach(element => {
-            result.push(element);
-        });    
-    }
-    if(!isNullOrUndefined(city)){
-        const listOfcity = await connection.find({city:{$regex:city,$options:"$i"}});
-        listOfcity.forEach(element => {
-            result.push(element);
-        });    
-    }
-    if(!isNullOrUndefined(minPackage) && minPackage > 0){
-        const listOfminPackage = await connection.find({minPackage:{$gte : minPackage}});
-        listOfminPackage.forEach(element => {
-            result.push(element);
-        });    
-    }
-    if(!isNullOrUndefined(maxFees) && maxFees > 0){
-        const listOfmaxFees = await connection.find({maxFees:{$lte : maxFees}});
-        listOfmaxFees.forEach(element => {
-            result.push(element);
-        });    
-    }
-    if(!isNullOrUndefined(course)){
-        const listOfcourse = await connection.find({course:{$regex:course,$options:"$i"}});
-        listOfcourse.forEach(element => {
-            result.push(element);
-        });    
-    }
-    if(!isNullOrUndefined(exam)){
-        const listOfexams = await connection.find({exam:{$regex:exam,$options:"$i"}});
-        listOfexams.forEach(element => {
-            result.push(element);
-        });    
-    }
-    res.send([...new Set(result)]);
 })
 
 
